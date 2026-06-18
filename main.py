92f58d00d667df1fa0cba791f6b5d10a52790eb8
fsalahi <53266505+fsalahi@users.noreply.github.com>
2026-06-18 15:50:11 -0400

    Added more functions to the menu

--- main.py
+++ main.py
@@ -2,36 +2,6 @@
 from models.book import Book
 from services.library import Library
 
-# # initialize test
-# book1 = Book(
-#     "A Story",
-#     "Fatima Salahi",
-#     2022
-# )
-# book2 = Book(
-#     "Another Story",
-#     "Fatima Salahi",
-#     2023
-# )
-# # do not add book3 to the library
-# book3 = Book(
-#     "goast book",
-#     "no author",
-#     2023
-# )
-# myLib = Library("")
-# myLib.add_book(book1)
-# myLib.add_book(book2)
-
-# myLib.show_books()
-# print("All available books: ", myLib.available_books())
-# print(myLib.search_book("st22ory"))
-
-# test remove
-# myLib.remove_book(book2)
-# myLib.show_books()
-# print("All available books: ", myLib.available_books())
-
 #myLib.save_books() # after running this once I commented the code above
 myLib = Library("")
 myLib.load_books() # because this will load the books from json file
@@ -59,26 +29,50 @@ while True:
         year = int(input("Year: "))
         book = Book(title, author, year, True)
         myLib.add_book(book)
+        print("The book is added to the library.")
+
     elif choice == "2":
         myLib.show_books()
+
     elif choice == "3":
         print("Borrow book: ")
         title = input("Title: ")
-        searched_book = myLib.search_book(title)
-        if len(searched_book) > 1:
-            print("Here are the books with your search, please be more specific.")
-            for book in searched_book:
-                print(book.title)
-        elif len(searched_book) == 1 and searched_book[0].available:
-            searched_book[0].borrow()
-            print("You borrowed the book: ", searched_book[0].title, " for 2 weeks!")
-        elif len(searched_book) == 1 and not searched_book[0].available:
-            print("This book is not available at the moment.")
-        elif len(searched_book) == 0:
-            print("Sorry! Our library doesn't have this book.")  
+        selected_book = myLib.choose_one_book(title)
+        if selected_book and selected_book.available:
+            selected_book.borrow()
+            print("You borrowed the book: ", selected_book.title, " for 2 weeks!")
+        elif selected_book and not selected_book.available:
+            print("Currently this book is not available to borrow!")
+
+    elif choice == "4":
+        print("Return book: ")
+        title = input("Title: ")
+        selected_book = myLib.choose_one_book(title)
+        if selected_book:
+            selected_book.return_book()
+            print("You returned the book [", selected_book.title, "]. Thanks!")
+
+    elif choice == "5":
+        title = input("What is the title you are looking for? ")
+        matching_books = myLib.search_book(title)
+        if len(matching_books) >= 1:
+            print("Here are the books with your search:")
+            for book in matching_books:
+                print(
+                    f"Title: {book.title} \t| "
+                    f"Author: {book.author} \t| "
+                    f"Available: {book.available}")
+        else:
+            print("There was no result for this search!")
+
+    elif choice == "6":
+        myLib.save_books()
+        print("Books are saved!")
+
     elif choice == "7":
         myLib.save_books()
-        print("Hope to see you soon!")
+        print("Goodbye!")
         break
+
     else:
         print("Error")
